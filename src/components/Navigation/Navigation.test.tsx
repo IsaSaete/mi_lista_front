1e68847c1f24cab83acaa961e6cc4fa448238d6d
Isabel Sáenz <isasaete@gmail.com>
2025-07-17 12:43:38 +0200

    Add test to Navigation component

---
+++ src/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import Navigation from "./Navigation";
+import { MemoryRouter } from "react-router";
+
+describe("Given the Navigation component", () => {
+  describe("When it renders", () => {
+    test("Then it should show a 'MENÚ SEMANAL' link", () => {
+      const expectedLink = /menú semanal/i;
+
+      render(<Navigation />, { wrapper: MemoryRouter });
+
+      const menuLink = screen.getByRole("link", { name: expectedLink });
+
+      expect(menuLink).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'MI CESTA' link", () => {
+      const expectedLink = /mi cesta/i;
+
+      render(<Navigation />, { wrapper: MemoryRouter });
+
+      const cartLink = screen.getByRole("link", { name: expectedLink });
+
+      expect(cartLink).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'RECETAS' link", () => {
+      const expectedLink = /recetas/i;
+
+      render(<Navigation />, { wrapper: MemoryRouter });
+
+      const menuLink = screen.getByRole("link", { name: expectedLink });
+
+      expect(menuLink).toBeInTheDocument();
+    });
+  });
+});

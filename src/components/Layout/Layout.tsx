44cd8a943d11719ea6728d8e80c283580e051d99
Isabel Sáenz <isasaete@gmail.com>
2025-07-17 12:44:05 +0200

    Add Layout & App component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import Navigation from "../Navigation/Navigation";
+import "./Layout.css";
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <div className="main-container">
+        <Navigation />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/styles.css";
+import App from "./components/App/App";
+import "@fontsource-variable/quicksand/index.css";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="main-container"></div>
-  </StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );

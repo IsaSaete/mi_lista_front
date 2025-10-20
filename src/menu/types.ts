90c4db00ab72e356f783e8be931ac53b4c1bf307
Isabel Sáenz <isasaete@gmail.com>
2025-10-20 20:46:38 +0200

    Add IngredientForm component

--- src/menu/types.ts
+++ src/menu/types.ts
@@ -1,8 +1,18 @@
+import type { IngredientDto } from "./shoppingList/dto/type";
+
 export type MenuType = "comida" | "cena";
 
 export interface Ingredient {
   id: string;
   name: string;
   category: string;
+  purchasedAt: boolean;
   createdAt: Date;
 }
+
+export interface ShoppingList {
+  _id: string;
+  ingredients: IngredientDto[];
+}
+
+export type NewIngredient = Omit<Ingredient, "id" | "category" | "createdAt">;

25149e2ef9db31d2021de1a09b33bb7bc593916a
Isabel Sáenz <isasaete@gmail.com>
2025-10-28 18:35:21 +0100

    Add test of togglePurchasedStatus function to useShoppingList

---
+++ src/menu/shoppingList/hooks/__tests__/togglePurchasedStatus.test.tsx
@@ -0,0 +1,39 @@
+import { tomato } from "@/mockData/ingredients";
+import type { ShoppingListState } from "../../slice/types";
+import setupStore from "@/store/setupStore";
+import { MemoryRouter } from "react-router";
+import { Provider } from "react-redux";
+import { act, renderHook } from "@testing-library/react";
+import useShoppingList from "../useShoppingList";
+
+describe("Given the togglePurchasedStatus function", () => {
+  describe("When it's called with Tomato's id ingredient that is not purchased", () => {
+    test("Then it should update Tomato ingredient and it's purchased", async () => {
+      const initialState: ShoppingListState = {
+        ingredients: [tomato],
+      };
+
+      const store = setupStore({ shoppingList: initialState });
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <MemoryRouter>
+          <Provider store={store}>{children}</Provider>
+        </MemoryRouter>
+      );
+
+      const { result } = renderHook(() => useShoppingList(), {
+        wrapper: wrapper,
+      });
+
+      await act(async () => {
+        result.current.togglePurchasedStatus(tomato.id);
+      });
+
+      const ingredients = result.current.ingredients;
+
+      expect(ingredients).toContainEqual(
+        expect.objectContaining({ name: tomato.name, isPurchased: false }),
+      );
+    });
+  });
+});

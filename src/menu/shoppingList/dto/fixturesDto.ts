9f93fd3e070423b865d6ebf333cc851f3b11df4b
Isabel Sáenz <isasaete@gmail.com>
2025-10-28 12:11:03 +0100

    Change purchasedAt to isPurchased

--- src/menu/shoppingList/dto/fixturesDto.ts
+++ src/menu/shoppingList/dto/fixturesDto.ts
@@ -5,7 +5,7 @@ export const tomatoDto: IngredientDto = {
   name: "Tomate",
   category: "Verdura",
   createdAt: "2025-10-01T10:00:00Z",
-  purchasedAt: false,
+  isPurchased: false,
 };
 
 export const aceiteDto: IngredientDto = {
@@ -13,28 +13,28 @@ export const aceiteDto: IngredientDto = {
   name: "Aceite de oliva virgen extra",
   category: "Aceite",
   createdAt: "2025-10-02T12:15:00Z",
-  purchasedAt: false,
+  isPurchased: false,
 };
 export const espaguetiDto: IngredientDto = {
   _id: "3",
   name: "Pasta espagueti",
   category: "Cereal",
   createdAt: "2025-10-03T09:30:00Z",
-  purchasedAt: false,
+  isPurchased: false,
 };
 export const albahacaDto: IngredientDto = {
   _id: "4",
   name: "Albahaca",
   category: "Hierba aromática",
   createdAt: "2025-10-05T08:45:00Z",
-  purchasedAt: false,
+  isPurchased: false,
 };
 export const parmesanoDto: IngredientDto = {
   _id: "5",
   name: "Queso parmesano",
   category: "Lácteo",
   createdAt: "2025-10-06T17:20:00Z",
-  purchasedAt: false,
+  isPurchased: false,
 };
 
 export const ingredientsDto: IngredientDto[] = [

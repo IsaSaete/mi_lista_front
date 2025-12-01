edc384b03054d47a23663cc8b2c9199f77c131d7
Isabel Sáenz <isasaete@gmail.com>
2025-12-01 17:55:59 +0100

    Add RegisterForm component and its tests

---
+++ src/auth/components/RegisterForm/RegisterForm.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import RegisterForm from "./RegisterForm";
+
+describe("Given the RegisterForm component", () => {
+  const action = vitest.fn();
+
+  describe("When it renders", () => {
+    test("Then it should show a 'Nombre' text box", () => {
+      const expectedText = /nombre/i;
+
+      render(<RegisterForm onTabChange={action} />);
+
+      const nameTextBox = screen.getByLabelText(expectedText);
+
+      expect(nameTextBox).toBeInTheDocument();
+    });
+
+    describe("And the user types 'Francisca' in the 'Nombre' text box", () => {
+      test("Then it should show Francisca to enter the name", async () => {
+        const expectedText = /nombre/i;
+        const userName = "Francisca";
+
+        render(<RegisterForm onTabChange={action} />);
+
+        const nameTextBox = screen.getByLabelText(expectedText);
+
+        await userEvent.type(nameTextBox, userName);
+
+        expect(nameTextBox).toHaveValue(userName);
+      });
+    });
+
+    test("Then it should show a 'Email' text box", () => {
+      const expectedText = /email/i;
+
+      render(<RegisterForm onTabChange={action} />);
+
+      const emailTextBox = screen.getByLabelText(expectedText);
+
+      expect(emailTextBox).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'Contraseña' tet box", () => {
+      const expectedText = /contraseña/i;
+
+      render(<RegisterForm onTabChange={action} />);
+
+      const nameTextBox = screen.getByLabelText(expectedText);
+
+      expect(nameTextBox).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'Regístrate' button text", () => {
+      const expectedText = /regístrate/i;
+
+      render(<RegisterForm onTabChange={action} />);
+
+      const buttonText = screen.getByRole("button", { name: expectedText });
+
+      expect(buttonText).toBeInTheDocument();
+    });
+  });
+});

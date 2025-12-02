5b12b878ed5fc21f7ee40f1c9a0484623cbf97af
Isabel Sáenz <isasaete@gmail.com>
2025-12-02 11:40:11 +0100

    Add Footer component and its test

---
+++ src/UI/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full p-5 text-center mt-auto flex flex-col">
+      <div>
+        <div className="flex justify-center gap-3 mb-2">
+          <span className="text-xl">♻️</span>
+          <span className="text-xl">🍃</span>
+          <span className="text-xl">🌍</span>
+        </div>
+        <p className="text-secondary-hover font-semibold text-sm">
+          Pequeños planes, grandes impactos
+        </p>
+        <p className="text-foreground text-xs mt-1">
+          Mi Menú © {new Date().getFullYear()}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

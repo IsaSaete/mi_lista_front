2ccd7f7dc7448e0354aa3f06df179d4112ac6f67
Isabel Sáenz <isasaete@gmail.com>
2025-12-03 13:08:31 +0100

    Create authSlice and add to store

---
+++ src/auth/slice/authSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { AuthState } from "./types";
+import type { AuthResponse } from "../types";
+
+const initialState: AuthState = {
+  isLoading: false,
+  userInfo: null,
+  token: null,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    registerUserStart: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    registerUserSuccess: (state, action: PayloadAction<AuthResponse>) => {
+      state.isLoading = false;
+      state.token = action.payload.token;
+      state.userInfo = action.payload.user;
+    },
+    registerUserFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.error = null;
+      state.token = null;
+      state.userInfo = null;
+    },
+  },
+});
+
+export const {
+  registerUserStart: registerUserStartCreator,
+  registerUserSuccess: registerUserCreator,
+  registerUserFailure: registerUserFailureCreator,
+  logout,
+} = authSlice.actions;
+
+export const authReducer = authSlice.reducer;
--- src/store/setupStore.ts
+++ src/store/setupStore.ts
@@ -3,10 +3,13 @@ import { shoppingListReducer } from "@/menu/shoppingList/slice/shoppingListSlice
 import type { ShoppingListState } from "@/menu/shoppingList/slice/types";
 import { weeklyMenuReducer } from "@/menu/weeklyMenu/slice/weeklyMenuSlice";
 import type { WeeklyMenuState } from "@/menu/weeklyMenu/slice/types";
+import type { AuthState } from "@/auth/slice/types";
+import { authReducer } from "@/auth/slice/authSlice";
 
 export type RootState = {
   shoppingList: ShoppingListState;
   weeklyMenu: WeeklyMenuState;
+  auth: AuthState;
 };
 
 export const setupStore = (preloadedState?: Partial<RootState>) => {
@@ -14,6 +17,7 @@ export const setupStore = (preloadedState?: Partial<RootState>) => {
     reducer: {
       shoppingList: shoppingListReducer,
       weeklyMenu: weeklyMenuReducer,
+      auth: authReducer,
     },
     preloadedState: preloadedState as RootState,
   });

1767f9cd31ab230599fc0b1105c852423c5da9c7
Isabel Sáenz <isasaete@gmail.com>
2025-12-15 18:39:35 +0100

    Modify AppRouter and add protected routes

--- src/router/AppRouter.test.tsx
+++ src/router/AppRouter.test.tsx
@@ -4,15 +4,23 @@ import { Provider } from "react-redux";
 import AppRouter from "./AppRouter";
 import store from "@/store/store";
 
-describe("Given the AppRouter component", () => {
-  describe("when it renders in path /recets that it doesn't exists", () => {
+afterEach(() => {
+  vi.clearAllMocks();
+  vi.restoreAllMocks();
+});
+
+describe("Given the AppRouter component with", () => {
+  describe("when it renders with autheticated user in path /recetsa that it doesn't exists", () => {
     test("Then it should show a 'Página no encontrada' inside a heading", async () => {
+      Storage.prototype.getItem = vi.fn().mockReturnValue("mock-token-123");
       const expectedTitlePage = /página no encontrada/i;
 
       render(
-        <MemoryRouter initialEntries={["/recers"]}>
-          <AppRouter />
-        </MemoryRouter>,
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/recers"]}>
+            <AppRouter />
+          </MemoryRouter>
+        </Provider>,
       );
 
       const titlePage = await screen.findByRole("heading", {
@@ -23,8 +31,9 @@ describe("Given the AppRouter component", () => {
     });
   });
 
-  describe("When it render in path /lista-compra", () => {
+  describe("When it render with autheticated user in path /lista-compra", () => {
     test("Then it should show a 'LISTA DE LA COMPRA' inside a heading", async () => {
+      Storage.prototype.getItem = vi.fn().mockReturnValue("mock-token-123");
       const expectedTitlePage = /lista de la compra/i;
 
       render(
@@ -40,6 +49,25 @@ describe("Given the AppRouter component", () => {
       });
 
       expect(titlePage).toBeInTheDocument();
+    });
+  });
+
+  describe("When it render with no autheticated user in path /lista-compra", () => {
+    test("Then it should redirect to /auth when accessing protected route", async () => {
+      const expectedText = /iniciar sesión/i;
+
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/recers"]}>
+            <AppRouter />
+          </MemoryRouter>
+        </Provider>,
+      );
+
+      const loginButton = await screen.findByRole("button", {
+        name: expectedText,
+      });
+      expect(loginButton).toBeInTheDocument();
     });
   });
 });
